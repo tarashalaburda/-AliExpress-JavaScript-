dfc147e1068ce0f03a1dc1cad4212c9faea628e1
Taras Halaburda <tarashalaburda@gmail.com>
2019-02-19 17:29:38 +0200

    add and wrapped the code in the event handler

--- js/script.js
+++ js/script.js
@@ -1,45 +1,49 @@
-const cartWrapper = document.querySelector('.cart__wrapper'),
-      cart = document.querySelector('.cart'),
-      close = document.querySelector('.cart__close'), // for closing cross
-      open = document.querySelector('#cart'),
-      goodsBtn = document.querySelectorAll('.goods__btn'),
-      products = document.querySelectorAll('.goods__item'),
-      confirm = document.querySelector('.confirm'),
-      badge = document.querySelector('.nav__badge'), // quantity of goods
-      totalCost = document.querySelector('.cart_total > span'),
-      titles = document.querySelectorAll('.goods__title');
-    
+window.addEventListener('DOMContentLoaded', () => {
+    const cartWrapper = document.querySelector('.cart__wrapper'),
+    cart = document.querySelector('.cart'),
+    close = document.querySelector('.cart__close'), // for closing cross
+    open = document.querySelector('#cart'),
+    goodsBtn = document.querySelectorAll('.goods__btn'),
+    products = document.querySelectorAll('.goods__item'),
+    confirm = document.querySelector('.confirm'),
+    badge = document.querySelector('.nav__badge'), // quantity of goods
+    totalCost = document.querySelector('.cart_total > span'),
+    titles = document.querySelectorAll('.goods__title');
+  
 function openCart() {
-    cart.style.display = 'block';
-    document.body.style.overflow = 'hidden'; // closes other page elements
+  cart.style.display = 'block';
+  document.body.style.overflow = 'hidden'; // closes other page elements
 }
 
 function closeCart() {
-    cart.style.display = 'none';
-    document.body.style.overflow = '';
+  cart.style.display = 'none';
+  document.body.style.overflow = '';
 }
 
 open.addEventListener('click', openCart);
 close.addEventListener('click', closeCart);
 
 goodsBtn.forEach(function(btn, i) {
-    btn.addEventListener('click', () => {
-        let item = products[i].cloneNode(true),
-            trigger = item.querySelector('button'),
-            removeBtn = document.createElement('div'),
-             empty = cartWrapper.querySelector('.empty');
-        
-        trigger.remove();
+  btn.addEventListener('click', () => {
+      let item = products[i].cloneNode(true),
+          trigger = item.querySelector('button'),
+          removeBtn = document.createElement('div'),
+           empty = cartWrapper.querySelector('.empty');
+      
+      trigger.remove();
 
-        removeBtn.classList.add('goods__item-remove');
-        removeBtn.innerHTML = '&times'; 
-        item.appendChild(removeBtn);
-        
-        cartWrapper.appendChild(item);
-        if (empty) {
-            empty.remove();
-            
-        }
-        
-    });
+      removeBtn.classList.add('goods__item-remove');
+      removeBtn.innerHTML = '&times'; 
+      item.appendChild(removeBtn);
+      
+      cartWrapper.appendChild(item);
+      if (empty) {
+          empty.remove();
+
+      }
+      
+  });
 });
+});
+
+
