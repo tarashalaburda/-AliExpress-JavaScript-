2cc72a385b57b0f83368d2751a416a1f0177c830
Taras Halaburda <tarashalaburda@gmail.com>
2019-02-19 14:50:28 +0200

    add the main variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const cartWrapper = document.querySelector('.cart__wrapper'),
+      cart = document.querySelector('.cart'),
+      close = document.querySelector('cart__close'), // for closing cross
+      open = document.querySelector('#cart'),
+      goodsBtn = document.querySelectorAll('.goods__btn'),
+      products = document.querySelectorAll('.goods__item'),
+      confirm = document.querySelector('.confirm'),
+      badge = document.querySelector('.nav__badge'), // quantity of goods
+      totalCost = document.querySelector('.cart_total > span'),
+      titles = document.querySelectorAll('.goods__title');
+    
+
+
