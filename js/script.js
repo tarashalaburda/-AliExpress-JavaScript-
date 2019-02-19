7e817efa2318892db4205b411db003658db32ca0
Taras Halaburda <tarashalaburda@gmail.com>
2019-02-19 16:58:00 +0200

    add a product to the cart

--- js/script.js
+++ js/script.js
@@ -21,3 +21,25 @@ function closeCart() {
 
 open.addEventListener('click', openCart);
 close.addEventListener('click', closeCart);
+
+goodsBtn.forEach(function(btn, i) {
+    btn.addEventListener('click', () => {
+        let item = products[i].cloneNode(true),
+            trigger = item.querySelector('button'),
+            removeBtn = document.createElement('div'),
+             empty = cartWrapper.querySelector('.empty');
+        
+        trigger.remove();
+
+        removeBtn.classList.add('goods__item-remove');
+        removeBtn.innerHTML = '&times'; 
+        item.appendChild(removeBtn);
+        
+        cartWrapper.appendChild(item);
+        if (empty) {
+            empty.remove();
+            
+        }
+        
+    });
+});
