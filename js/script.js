809548c63fcb52c7424deabf191f2f80f23a5052
Taras Halaburda <tarashalaburda@gmail.com>
2019-02-19 15:55:05 +0200

    add Event Listener and fix one error

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const cartWrapper = document.querySelector('.cart__wrapper'),
       cart = document.querySelector('.cart'),
-      close = document.querySelector('cart__close'), // for closing cross
+      close = document.querySelector('.cart__close'), // for closing cross
       open = document.querySelector('#cart'),
       goodsBtn = document.querySelectorAll('.goods__btn'),
       products = document.querySelectorAll('.goods__item'),
@@ -9,5 +9,15 @@ const cartWrapper = document.querySelector('.cart__wrapper'),
       totalCost = document.querySelector('.cart_total > span'),
       titles = document.querySelectorAll('.goods__title');
     
+function openCart() {
+    cart.style.display = 'block';
+    document.body.style.overflow = 'hidden'; // closes other page elements
+}
 
+function closeCart() {
+    cart.style.display = 'none';
+    document.body.style.overflow = '';
+}
 
+open.addEventListener('click', openCart);
+close.addEventListener('click', closeCart);
