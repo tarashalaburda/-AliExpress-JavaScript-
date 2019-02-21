333d5ae50afa76af5b215e0d25ef1d092f697955
Taras Halaburda <tarashalaburda@gmail.com>
2019-02-21 17:14:57 +0200

    add a function that calculates the amount of an item in the cart

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
+
     const cartWrapper = document.querySelector('.cart__wrapper'),
         cart = document.querySelector('.cart'),
         close = document.querySelector('.cart__close'), // for closing cross
@@ -7,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
         products = document.querySelectorAll('.goods__item'),
         confirm = document.querySelector('.confirm'),
         badge = document.querySelector('.nav__badge'), // quantity of goods
-        totalCost = document.querySelector('.cart_total > span'),
+        totalCost = document.querySelector('.cart__total > span'),
         titles = document.querySelectorAll('.goods__title');
   
         function openCart() {
@@ -23,75 +24,91 @@ window.addEventListener('DOMContentLoaded', () => {
         open.addEventListener('click', openCart);
         close.addEventListener('click', closeCart);
 
-        goodsBtn.forEach(function(btn, i) {
-        btn.addEventListener('click', () => {
-            let item = products[i].cloneNode(true),
-                trigger = item.querySelector('button'),
-                removeBtn = document.createElement('div'),
-                empty = cartWrapper.querySelector('.empty');
+            goodsBtn.forEach(function(btn, i) {
+            btn.addEventListener('click', () => {
+                let item = products[i].cloneNode(true),
+                    trigger = item.querySelector('button'),
+                    removeBtn = document.createElement('div'),
+                    empty = cartWrapper.querySelector('.empty');
+                
+                trigger.remove();
+
+                showConfirm(); // shopping cart 
+
+                calcGoods(1); // display the quantity of goods in the red badge
+        
+                removeBtn.classList.add('goods__item-remove');
+                removeBtn.innerHTML = '&times'; 
+                item.appendChild(removeBtn);
+                
+                cartWrapper.appendChild(item);
+                if (empty) {
+                    empty.remove();
+
+                }
             
-            trigger.remove();
-
-            showConfirm(); // shopping cart 
-
-            calcGoods(1); // display the quantity of goods in the red badge
-
-            removeBtn.classList.add('goods__item-remove');
-            removeBtn.innerHTML = '&times'; 
-            item.appendChild(removeBtn);
+                calcTotal(); // calculation of the amount of goods
             
-            cartWrapper.appendChild(item);
-            if (empty) {
-                empty.remove();
-
-            }
-            
+                
+            });
         });
-    });
 
-    // function to reduce information about the product 
-    function sliceTitle() {
-        titles.forEach(function(item){
-            if (item.textContent.length < 60) {
-                return;
+        // function to reduce information about the product 
+        function sliceTitle() {
+            titles.forEach(function(item){
+                if (item.textContent.length < 60) {
+                    return;
 
-            } else { 
-                const str = item.textContent.slice(0, 61) + '...';
-                //const str = `${item.textContent.slice(0, 61)}...`;
-                item.textContent = str;
+                } else { 
+                    const str = item.textContent.slice(0, 61) + '...';
+                    //const str = `${item.textContent.slice(0, 61)}...`;
+                    item.textContent = str;
+                }
+            });
+        }
+        sliceTitle();
+
+        // function to create a shopping cart 
+        function showConfirm() {
+            confirm.style.display = 'block';
+            let counter = 100;
+            const id = setInterval(frame, 10);
+
+            function frame() {
+                if ( counter == 10) {
+                    clearInterval(id);
+                    confirm.style.display = 'none';
+                } else {
+                    counter--;
+                    
+                    confirm.style.opacity = '.' + counter;
+                    confirm.style.transform = `translateY(-${counter}px)`;
+                }
+                
+
             }
-        });
-    }
-    sliceTitle();
-
-    // function to create a shopping cart 
-    function showConfirm () {
-        confirm.style.display = 'block';
-        let counter = 100;
-        const id = setInterval(frame, 10);
-
-        function frame() {
-            if ( counter == 10) {
-                clearInterval(id);
-                confirm.style.display = 'none';
-            } else {
-                counter--;
-                confirm.style.transform = `translateY(-${counter}px)`;
-                confirm.style.opacity = '.' + counter;
-            }
-            
 
         }
+        // function to display the quantity of goods in the shopping cart in the badge
+        function calcGoods(i) {
+            const items = cartWrapper.querySelectorAll('.goods__item');
+            badge.textContent = i + items.length;
+        }
 
-    }
-    // function to display the quantity of goods in the shopping cart in the badge
-    function calcGoods(i) {
-        const items = cartWrapper.querySelectorAll('.goods__item');
-        badge.textContent = i + items.length;
-    }
-      
-    
-    
+        //function to calculate the amount of goods in the shopping cart
+        function calcTotal() {
+            const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+            let total = 0;
+            prices.forEach(function(item) {
+                total += +item.textContent; // convert string to number 
+            });
+
+            totalCost.textContent = total;
+
+        
+        }
+           
+
 
 
 });
